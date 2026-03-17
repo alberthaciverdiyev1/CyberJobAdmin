8a9c15313a19c5cf3c35d37468bd241ae8ca8789
albert <alberthaciverdiyev55@gmail.com>
2026-03-18 00:06:11 +0400

    added Partner resource with CRUD functionality

---
+++ app/Filament/Resources/Partners/Schemas/PartnerForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\Partners\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class PartnerForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                FileUpload::make('image')
+                    ->image()
+                    ->imageEditor()
+                    ->required()
+                ->columnSpanFull(),
+                TextInput::make('link'),
+                Toggle::make('is_active')
+                    ->default(true)
+                    ->required(),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Partners/Tables/PartnersTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources\Partners\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ReplicateAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class PartnersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('image'),
+                TextColumn::make('link')
+                    ->searchable(),
+                IconColumn::make('is_active')
+                    ->boolean(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+                ReplicateAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
